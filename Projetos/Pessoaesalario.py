361e74ca87e5d37988dff008b157e5c5871ba2f4
vittorliebe <vittorbernardo2@gmail.com>
2025-07-10 20:32:02 -0300

    Create Pessoaesalario.py

---
+++ Projetos/Pessoaesalario.py
@@ -0,0 +1,9 @@
+codigo = int(input("Digite o código: "))
+nome  = input("Digite o nome: ")
+salario = float(input("Digite o salário: "))
+ativo = True
+
+print("Código: \n %d " % codigo)
+print("Nome: \n %s " % nome)
+print("Salário: \n %.2f "% salario)
+print("Ativo: %r" % ativo)
